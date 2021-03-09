a3b60f33a6899e852404cae8fedd871142fcc769
starleybatera <starley.batera@hotmail.com>
2021-03-08 22:50:44 -0300

    alter files

--- streamapp/views.py
+++ streamapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http.response import StreamingHttpResponse
-from streamapp.camera import VideoCamera, IPWebCam, MaskDetect, LiveWebCam
+from streamapp.camera import IPWebCam
 # Create your views here.
 
 
@@ -15,20 +15,7 @@ def gen(camera):
 				b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 
-def video_feed(request):
-	return StreamingHttpResponse(gen(VideoCamera()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
-
-
 def webcam_feed(request):
 	return StreamingHttpResponse(gen(IPWebCam()),
 					content_type='multipart/x-mixed-replace; boundary=frame')
 
-
-def mask_feed(request):
-	return StreamingHttpResponse(gen(MaskDetect()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
-					
-def livecam_feed(request):
-	return StreamingHttpResponse(gen(LiveWebCam()),
-					content_type='multipart/x-mixed-replace; boundary=frame')
